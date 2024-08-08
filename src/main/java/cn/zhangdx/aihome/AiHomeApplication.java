6a06fea48612dbcf4f504751cb04884cbf9a67df
zhangdx <dongxu.zhang@ideamake.cn>
2024-08-08 17:06:59 +0800

    feat:初始化ai-home项目

---
+++ src/main/java/cn/zhangdx/aihome/AiHomeApplication.java
@@ -0,0 +1,17 @@
+package cn.zhangdx.aihome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Ai-Home公众号服务端应用
+ * @author zhangdx
+ * @date 2024/8/8 15:40
+ **/
+@SpringBootApplication
+public class AiHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiHomeApplication.class, args);
+    }
+}
---
+++ src/main/java/cn/zhangdx/aihome/controller/MpCheckController.java
@@ -0,0 +1,37 @@
+package cn.zhangdx.aihome.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author zhangdx
+ * @date 2024/8/8 15:59
+ **/
+@Slf4j
+@RestController
+@RequestMapping("/wx")
+public class MpCheckController {
+
+    @Value("${mp.config.token}")
+    private String mpConfigToken;
+
+    @GetMapping
+    public String checkApi(@RequestParam String signature, @RequestParam String timestamp, @RequestParam String nonce,
+                         @RequestParam String echostr) {
+        log.info("MpCheckController checkApi signature:{}, timestamp:{}, nonce:{}, echostr:{}", signature, timestamp, nonce, echostr);
+        List<String> list = Arrays.asList(mpConfigToken, timestamp, nonce);
+        Collections.sort(list);
+        String signatureStr = String.join("", list);
+        log.info("MpCheckController checkApi signatureStr:{}", signatureStr);
+        return Objects.equals(signature, signatureStr) ? echostr : null;
+    }
+}
